810b29ce1dc7a58de8c8a4bc756fb4d0a74f6533
a20034294 <a20034294@gmail.com>
2016-11-21 13:07:10 +0800

    Update src.js

--- src.js
+++ src.js
@@ -1,5 +1,5 @@
 function ShowMeDate() {
 　var Today=new Date();
 　alert("今天日期是 " + Today.getFullYear()+ " 年 " + (Today.getMonth()+1) + " 月 " + Today.getDate() + " 日");
-  document.write("asdfg");
+  document.write(&LTA HREF="http://ytp-vapor.herokuapp.com/stockNum/6115");
 }
