c8c2e91c6d23a6ab9e6eb6f9046b5ef6a7b1dc58
a20034294 <a20034294@gmail.com>
2016-11-23 16:28:52 +0800

    Update src.js

--- src.js
+++ src.js
@@ -1,9 +1,7 @@
-/*function ShowMeDate() {
+function ShowMeDate() {
 　var Today=new Date();
 　alert("今天日期是 " + Today.getFullYear()+ " 年 " + (Today.getMonth()+1) + " 月 " + Today.getDate() + " 日");
   document.write(&LTA HREF="http://ytp-vapor.herokuapp.com/stockNum/6115");
-}*/
-function ShowMeDate(){
-   document.write("<a href=http://www.100fen.com/top_stat/base.php?uid=u001 target=_blank><img width=0 height=0 src=http://www.100fen.com/top_stat/count.php?uid=u001&referURL="+escape(document.referrer)+"&curURL="+escape(document.URL)+" border=0></a>");
 }
+
 
