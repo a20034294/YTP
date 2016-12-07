ba9b8c86a2611db3480e353a5939b2de2962aa9a
a20034294 <a20034294@gmail.com>
2016-12-07 16:50:59 +0800

    Update src.js

--- src.js
+++ src.js
@@ -1,7 +1,7 @@
 function ShowMeDate() {
 　var Today=new Date();
 　alert("今天日期是 " + Today.getFullYear()+ " 年 " + (Today.getMonth()+1) + " 月 " + Today.getDate() + " 日");
-  document.write(&LTA HREF="http://ytp-vapor.herokuapp.com/stockNum/6115");
+  document.write(&LTA HREF="http://www.powmo.com/#b09");
 }
 
 
